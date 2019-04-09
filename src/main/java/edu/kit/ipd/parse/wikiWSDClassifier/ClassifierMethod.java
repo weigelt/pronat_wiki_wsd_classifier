e7d249c66b49ee6ed30e2ebe557dfcb69b80ae0e
Tobias Hey <hey@kit.edu>
2019-04-09 17:09:41 +0200

    add jans version

---
+++ src/main/java/edu/kit/ipd/parse/wikiWSDClassifier/ClassifierMethod.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package edu.kit.ipd.parse.wikiWSDClassifier;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.bayes.NaiveBayes;
+
+/**
+ * Enum representing the various classifier methods like NaiveBayes.
+ *
+ * @author Jan Keim
+ *
+ */
+public enum ClassifierMethod {
+	EfficientNaiveBayes, NaiveBayes; // J48, RandomForest;
+
+	public Classifier getClassifier() {
+		switch (this) {
+		case EfficientNaiveBayes:
+			return new EfficientNaiveBayes();
+		case NaiveBayes:
+			return new NaiveBayes();
+		default:
+			return new EfficientNaiveBayes();
+		}
+	}
+}
